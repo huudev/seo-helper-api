c4cd486822435d9da646294283b4bfd5957c5fd4
huudev <vanhuudev@gmail.com>
2025-08-18 21:41:14 +0700

    update devserver work with no virual env

--- devserver.bat
+++ devserver.bat
@@ -1,1 +1,5 @@
-.\.venv\Scripts\activate.bat && uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+if exist ".\.venv\Scripts\activate.bat" (
+    .\.venv\Scripts\activate.bat && uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+) else (
+    uvicorn app.main:app --host 0.0.0.0 --port 8000
+)
